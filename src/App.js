2850b3b9d2f86d2ac816c005fedd29887ed60fb0
David McDonagh <david@leapfrogdbs.co.uk>
2023-05-09 18:06:39 +0100

    Build

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from "react"
 import './App.css';
+import Question from './components/Question'
+import { nanoid } from 'nanoid';
+
 
 
 function App() {
   const [questions, setQuestions] = React.useState([])
 
-  React.useEffect(() => {
+function startQuiz() {
     fetch("https://opentdb.com/api.php?amount=5")
     .then(res => res.json())
-    .then(data => setQuestions(data.results))
-  }, [])
+    .then(data => setQuestions(
+      data.results.map((question) => ({ ...question, id: nanoid() }))
+    ))
+  }
 
   return (
     <div className="App">
       
+      {!questions.length ? (
       <section className="start">
         <h1 className="startSceeen__title">Quizical</h1>
         <p className="start__description">Some Description if neeed</p>
-        <button className="start_button">Start Quiz</button>
+        <button onClick={startQuiz} className="start_button">Start Quiz</button>
       </section>
-
+      ) : (
       <section className="questions">
-          
+        {questions.map((question) =>
+          <Question key={question.id} question={question} id={question.id} />
+        )}
       </section>
-
+      )}
+      
 
     </div>
   );
---
+++ src/components/Question.js
@@ -0,0 +1,37 @@
+import React from "react"
+import he from 'he';
+
+export default function Question(props) {
+
+    const [selectedAnswer, setSelectedAnswer] = React.useState('');
+    const [isCorrect, setIsCorrect] = React.useState(false);
+    
+    const [answerChoices] = React.useState(() => {
+        const randomIndex = Math.floor(Math.random() * (props.question.incorrect_answers.length + 1));
+        const choices = [...props.question.incorrect_answers]      
+        choices.splice(randomIndex, 0, props.question.correct_answer) 
+        return choices;
+    })
+
+    const handleChange = (e) => {
+        setSelectedAnswer(e.target.value);
+        setIsCorrect(e.target.value === props.question.correct_answer) 
+    }
+                        
+    return (
+        <div className="question">
+           <h1>Question instance</h1>
+           <p>Question: {he.decode(props.question.question)}</p>
+           <p>Key: {props.question.id}</p>
+           <p>Answer: {props.question.correct_answer}</p>
+           <div className="answer-choices">
+              {answerChoices.map((choice, index) => (
+                <div className="answer-choice" key={index}>
+                    <input type="radio" id={`choice-${props.id}-${index}`} name={`${props.id}-answer`} value={he.decode(choice)} onChange={handleChange} />
+                    <label htmlFor={`choice-${props.id}-${index}`}>{he.decode(choice)}</label>
+                </div>
+              ))}
+           </div>
+        </div>
+    )
+}
